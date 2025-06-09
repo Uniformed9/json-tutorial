68267cfe00a0d87505533dce644f0dd10ca3594a
Uniformed9 <1498964461@qq.com>
2025-06-09 16:06:35 +0800

    myanser2

--- tutorial02/leptjson.c
+++ tutorial02/leptjson.c
@@ -1,8 +1,11 @@
 #include "leptjson.h"
 #include <assert.h>  /* assert() */
 #include <stdlib.h>  /* NULL, strtod() */
+#include<errno.h>
 
 #define EXPECT(c, ch)       do { assert(*c->json == (ch)); c->json++; } while(0)
+#define ISDIGIT(ch) ((ch)>='0'&&(ch)<='9')
+#define ISDIGIT1TO9(ch) ((ch)>='1'&&(ch)<='9')
 
 typedef struct {
     const char* json;
@@ -41,15 +44,75 @@ static int lept_parse_null(lept_context* c, lept_value* v) {
     v->type = LEPT_NULL;
     return LEPT_PARSE_OK;
 }
-
+static int lept_parse_number_judge(const char* str) {
+    const char* p = str;
+    int flag = 0;
+    if (*p=='-') {
+        p++;
+    }
+    //检查整数部分
+    if (*p == '0') {
+        p++;
+        flag = 1;
+    }
+    else if(ISDIGIT(*p)) {
+        while (ISDIGIT(*p)) {
+            p++;
+        }
+    }
+    else {
+        return 0;
+    }
+    //检查小数点,有小数点就必须有整数，这里可以有前导0
+    if (*p=='.') {
+        //先匹配数字，然后再匹配
+        p++;
+        if (ISDIGIT(*p)) {
+            while (ISDIGIT(*p)) {
+                p++;
+            }
+        }
+        else return 0;
+    }
+    if (*p == 'e'|| *p == 'E') {
+        p++;
+        if (*p == '-' || *p == '+') {
+            p++;
+        }
+        if (ISDIGIT(*p)) {
+            while (ISDIGIT(*p)) {
+                p++;
+            }
+        }
+    }
+    return *p == '\0'?1:(flag==1?2:0);
+}
 static int lept_parse_number(lept_context* c, lept_value* v) {
+    if (!(ISDIGIT(*c->json) || *c->json == '-'))return LEPT_PARSE_INVALID_VALUE;
     char* end;
+    double num;
+
+    errno = 0;
+    int ret = lept_parse_number_judge(c->json);
     /* \TODO validate number */
-    v->n = strtod(c->json, &end);
-    if (c->json == end)
+    if (ret==0) {
         return LEPT_PARSE_INVALID_VALUE;
-    c->json = end;
-    v->type = LEPT_NUMBER;
+    }
+    else if (ret == 2) {
+        return LEPT_PARSE_ROOT_NOT_SINGULAR;
+    }
+    else {
+        num= strtod(c->json, &end);
+        if (errno == ERANGE) {
+            if (num != 0) {
+                return LEPT_PARSE_NUMBER_TOO_BIG;
+            }
+           
+        }
+        v->n = num;
+        c->json = end;
+        v->type = LEPT_NUMBER;
+    }
     return LEPT_PARSE_OK;
 }
 
@@ -58,8 +121,9 @@ static int lept_parse_value(lept_context* c, lept_value* v) {
         case 't':  return lept_parse_true(c, v);
         case 'f':  return lept_parse_false(c, v);
         case 'n':  return lept_parse_null(c, v);
-        default:   return lept_parse_number(c, v);
         case '\0': return LEPT_PARSE_EXPECT_VALUE;
+        default:   
+            return lept_parse_number(c, v);
     }
 }
 
